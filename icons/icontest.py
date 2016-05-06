a100d8c03cb4851f5df13236538b201cab7ddbf6
Robert HH <robert@hammelrath.com>
2016-05-06 08:12:15 +0200

    Added a standard button made from circles for comparison

--- icons/icontest.py
+++ icons/icontest.py
@@ -18,3 +18,12 @@ mytft.drawBitmap(50,200, *radiobutton.get_icon(0))
 mytft.drawBitmap(100,200, *radiobutton.get_icon(1))
 mytft.drawBitmap(150,200, *radiobutton.get_icon(1))
 
+
+mytft.drawCircle(250, 216, 16, (150,150,150))
+mytft.drawCircle(250, 216, 15, (0,0,0))
+mytft.fillCircle(250, 216, 6, (0,255,0))
+mytft.drawCircle(300, 216, 16, (150,150,150))
+mytft.drawCircle(300, 216, 15, (0,0,0))
+mytft.drawCircle(350, 216, 16, (150,150,150))
+mytft.drawCircle(350, 216, 15, (0,0,0))
+
