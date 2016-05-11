c40102e940429a9fe5d19eccd600971f3cd595b2
Robert HH <robert@hammelrath.com>
2016-05-11 21:13:57 +0200

    Sample icons using more than one colortable

--- icons/icontest.py
+++ icons/icontest.py
@@ -5,24 +5,33 @@ import tft
 import checkbox
 import radiobutton
 import switch
-import sliderpath
-import sliderhandle
+import mdesign
 
 
 mytft = tft.TFT()
 mytft.backlight(100)
 mytft.clrSCR((255,255,255))
 
-checkbox.draw(20, 20, 0, mytft.drawBitmap)
-checkbox.draw(20, 60, 1, mytft.drawBitmap)
-checkbox.draw(20, 100, 2, mytft.drawBitmap)
+if True:
+    for color in range(4):
+        mdesign.draw(color * 60 + 20, 20,  0, mytft.drawBitmap, color)
+        mdesign.draw(color * 60 + 20, 60,  1, mytft.drawBitmap, color)
+        mdesign.draw(color * 60 + 20, 100, 2, mytft.drawBitmap, color)
+        mdesign.draw(color * 60 + 20, 140, 3, mytft.drawBitmap, color)
 
-radiobutton.draw(20, 150, 1, mytft.drawBitmap)
-radiobutton.draw(60, 150, 0, mytft.drawBitmap)
-radiobutton.draw(100, 150, 0, mytft.drawBitmap)
 
-switch.draw(150, 20, 0, mytft.drawBitmap)
-switch.draw(150, 60, 1, mytft.drawBitmap)
+if False:
+    mytft.clrSCR((255,255,255))
 
-sliderpath.draw(300, 130, 0, mytft.drawBitmap)
-sliderhandle.draw(300, 150, 1, mytft.drawBitmap)
+    checkbox.draw(20, 20, 0, mytft.drawBitmap)
+    checkbox.draw(20, 60, 1, mytft.drawBitmap)
+    checkbox.draw(20, 100, 2, mytft.drawBitmap)
+
+    radiobutton.draw(20, 150, 1, mytft.drawBitmap)
+    radiobutton.draw(60, 150, 0, mytft.drawBitmap)
+    radiobutton.draw(100, 150, 0, mytft.drawBitmap)
+
+    switch.draw(150, 20, 0, mytft.drawBitmap)
+    switch.draw(150, 60, 1, mytft.drawBitmap)
+
+
